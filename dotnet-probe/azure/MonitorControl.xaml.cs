0a203274de80eda448a499a6c5ba5b9027c640e9
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-10-10 05:04:01 +0200

    working appsettings.json

--- dotnet-probe/azure/MonitorControl.xaml.cs
+++ dotnet-probe/azure/MonitorControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using Serilog;
 
 namespace dotnet_probe.azure;
 
@@ -10,5 +11,14 @@ public partial class MonitorControl : UserControl
     public MonitorControl()
     {
         InitializeComponent();
+    }
+
+    private void SendMetricsButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        // TODO: Implement sending metrics to Azure Monitor
+        string key = KeyTextBox.Text;
+        string value = ValueTextBox.Text;
+        // For now, just log
+        Serilog.Log.Information("Sending metric: {Key} = {Value}", key, value);
     }
 }
