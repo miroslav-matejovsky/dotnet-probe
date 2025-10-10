23b7dd6daed763901e1a7b47dc2448f74913127b
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-10-10 05:23:26 +0200

    adding ingestion client

--- dotnet-probe/azure/MonitorControl.xaml.cs
+++ dotnet-probe/azure/MonitorControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Controls;
+using Azure.Identity;
+using Azure.Monitor.Ingestion;
 using Serilog;
 
 namespace dotnet_probe.azure;
@@ -16,9 +18,24 @@ public partial class MonitorControl : UserControl
     private void SendMetricsButton_Click(object sender, System.Windows.RoutedEventArgs e)
     {
         // TODO: Implement sending metrics to Azure Monitor
-        string key = KeyTextBox.Text;
-        string value = ValueTextBox.Text;
-        // For now, just log
-        Serilog.Log.Information("Sending metric: {Key} = {Value}", key, value);
+        var metrics = new List<(string Key, string Value)>
+        {
+            (Key1TextBox.Text, Value1TextBox.Text),
+            (Key2TextBox.Text, Value2TextBox.Text),
+            (Key3TextBox.Text, Value3TextBox.Text),
+            (Key4TextBox.Text, Value4TextBox.Text)
+        };
+
+        var endpoint = new Uri("<data_collection_endpoint_uri>");
+        var credential = new DefaultAzureCredential();
+        var client = new LogsIngestionClient(endpoint, credential);
+        
+        foreach (var (key, value) in metrics)
+        {
+            if (!string.IsNullOrWhiteSpace(key) && !string.IsNullOrWhiteSpace(value))
+            {
+                Log.Information("Sending metric: {Key} = {Value}", key, value);
+            }
+        }
     }
 }
