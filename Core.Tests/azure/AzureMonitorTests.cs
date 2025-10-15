b9393ea17cc3aff1fd94d1065b1033d71e55bd18
Miroslav Matejovsky <miroslav.matejovsky@vissim.no>
2025-10-15 04:46:34 +0200

    more azure monitor

--- Core.Tests/azure/AzureMonitorTests.cs
+++ Core.Tests/azure/AzureMonitorTests.cs
@@ -2,43 +2,85 @@
 using System.Text.Json;
 using Azure.Identity;
 using Azure.Monitor.Ingestion;
-using Core.azure;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 namespace Core.Tests.azure;
+
+public record AzureMonitorConfig(
+    string DataCollectionEndpointUri,
+    string RuleId,
+    string StreamName
+);
 
 [TestFixture]
 public class AzureMonitorTests
 {
-    
-    [Ignore("For exploratory testing only")]
-    [Test]
-    public void TestSendingLogs()
+    private static readonly AzureMonitorConfig _config;
+
+    static AzureMonitorTests()
     {
         var configPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "azure", "local.config.json");
         var json = File.ReadAllText(configPath);
         var config = JsonSerializer.Deserialize<AzureMonitorConfig>(json);
         Assert.That(config, Is.Not.Null);
-        var endpoint = new Uri(config.DataCollectionEndpointUri);
-        var credential = new DefaultAzureCredential();
-        var client = new LogsIngestionClient(endpoint, credential);
-        DateTimeOffset currentTime = DateTimeOffset.UtcNow;
+        _config = config!;
     }
-    
+
     [Ignore("For exploratory testing only")]
     [Test]
     public void TestSendingMetrics()
     {
-        var configPath = Path.Combine(TestContext.CurrentContext.TestDirectory, "azure", "local.config.json");
-        var json = File.ReadAllText(configPath);
-        var config = JsonSerializer.Deserialize<AzureMonitorConfig>(json);
-        Assert.That(config, Is.Not.Null);
-        var monitor = new AzureMonitor();
-        monitor.SendMetrics(new List<(string, string)>
-        {
-            ("AppMetric1", "15.3"),
-            ("AppMetric2", "23.5")
-        });
+        var metricsProvider = Sdk.CreateMeterProviderBuilder()
+            .AddAzureMonitorMetricExporter(options =>
+            {
+                options.ConnectionString = "<Your Connection String>";
+                options.StorageDirectory = "C:\\SomeDirectory";
+            })
+            .Build();
+        // var monitor = new AzureMonitor();
+        // monitor.SendMetrics(new List<(string, string)>
+        // {
+        // ("AppMetric1", "15.3"),
+        // ("AppMetric2", "23.5")
+        // });
     }
-    
-    
+
+
+    [Ignore("For exploratory testing only")]
+    [Test]
+    public void TestSendingLogs()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddOpenTelemetry(logging =>
+            {
+                logging.AddAzureMonitorLogExporter(options =>
+                {
+                    options.ConnectionString = "<Your Connection String>";
+                    options.StorageDirectory = "C:\\SomeDirectory";
+                });
+            });
+        });
+        var credential = new DefaultAzureCredential();
+        var endpoint = new Uri(_config.DataCollectionEndpointUri);
+        var client = new LogsIngestionClient(endpoint, credential);
+        DateTimeOffset currentTime = DateTimeOffset.UtcNow;
+    }
+
+    [Ignore("For exploratory testing only")]
+    [Test]
+    public void TestSendingTraces()
+    {
+        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddAzureMonitorTraceExporter(options =>
+            {
+                options.ConnectionString = "<Your Connection String>";
+                options.StorageDirectory = "C:\\SomeDirectory";
+            })
+            .Build();
+    }
 }
